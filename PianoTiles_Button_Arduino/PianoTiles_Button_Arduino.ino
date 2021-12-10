a9fef133a0298eb3fe1f5848c09844098203ecfb
Daniel Ivanovich <dan@ivanovi.ch>
2021-12-10 09:03:58 -0500

    Music updates

--- PianoTiles_Button_Arduino/PianoTiles_Button_Arduino.ino
+++ PianoTiles_Button_Arduino/PianoTiles_Button_Arduino.ino
@@ -1,8 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
-enum MenuState{start, pickingSong, inGame};
-enum MenuState menuState;
+int menuState = 0;
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -11,14 +10,77 @@ const int numButtons = 4;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Order: same as columns, from left to right (green, blue, yellow, white)
-const int buttonPins[numButtons] = {8, 9, 7, 6}; // black, blue, yellow, white
+const int buttonPins[numButtons] = {A0, A1, A2, A3}; // black, blue, yellow, white
 int buttonStatuses[numButtons] = {0,0,0,0};
+
+const int BuzzerPin = 8;
 
 int chosenSong = 0;
 
+// SONG STUFF
+const int REST = 100; // has to be unique
+
+// 3rd Octave
+const int NC3 = 131; const int NCS3 = 138; const int ND3 = 147; const int NDS3 = 155;
+const int NE3 = 165; const int NF3 = 175; const int NFS3 = 185; const int NG3 = 196;
+const int NGS3 = 208; const int NA3 = 220; const int NAS3 = 233; const int NB3 = 247;
+
+// 4th Octave (middle C)
+int NC4 = 262; int NCS4 = 277; int ND4 = 294;
+int NDS4 = 311; int NE4 = 330; int NF4 = 349;
+int NFS4 = 370; int NG4 = 392; int NGS4 = 415;
+int NA4 = 440; int NAS4 = 466; int NB4 = 494;
+
+// 5th Octave
+int NB5 = 988; int NAS5 = 932; int NA5 = 880;
+int NGS5 = 830; int NG5 = 784; int NFS5 = 740;
+int NF5 =  698; int NE5 =  659; int NDS5 = 622;
+int ND5 = 587; int NCS5 = 554; int NC5 = 523;
+
+// 6th Octave
+
+int NB6 = 1975; int NAS6 = 1865; int NA6 = 1760;
+int NGS6 = 1661; int NG6 = 1568; int NFS6 = 1480;
+int NF6 = 1397; int NE6 = 1318; int NDS6 = 1244;
+int ND6 = 1175; int NCS6 = 1109; int NC6 = 1046;
+
+int song_bpm = 120; 
+int quarter = 60000 / song_bpm;
+int dottedQuarter = quarter * 1.5;
+int eighth = quarter / 2;
+int dottedEighth = eighth * 1.5;
+int half = 2 * quarter;
+int dottedHalf = 3 * quarter;
+int sixteenth = eighth / 2;
+
+int BellaCiao[44][3] = {
+    {NE5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NB5, eighth, eighth + 50}, {NC6, eighth, eighth + 50},
+    {NA5, eighth, eighth + 50}, {REST, dottedQuarter, 0}, {NE5, eighth, eighth + 50}, {NA5, eighth, eighth + 50},
+    {NB5, eighth, eighth + 50}, {NC6, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {REST, dottedQuarter, 0},
+
+    {NE5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NB5, eighth, eighth + 50}, {NC6, quarter, quarter + 50}, // last one is 1.3. CIAO
+    {NB5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NC6, quarter, quarter + 50},
+    {NB5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NE6, quarter, quarter + 50}, {NE6, quarter, quarter + 50}, {NE6, quarter, quarter + 50}, // CIAO CIAO CIAO. 24 notes
+  
+    {ND6, eighth, eighth + 50}, {NE6, eighth, eighth + 50}, {NF6, eighth, eighth + 50}, {NF6, dottedQuarter, dottedQuarter + 50}, {REST, dottedQuarter, 0},
+    {NF6, eighth, eighth + 50}, {NE6, eighth, eighth + 50}, {ND6, eighth, eighth + 50}, {NF6, eighth, eighth + 50}, {NE6, dottedQuarter,dottedQuarter  + 50}, {REST, quarter, 0},
+    {ND6, eighth, eighth + 50}, {NC6, eighth, eighth + 50}, {NB5, quarter, quarter + 50}, {NE6, quarter, quarter + 50}, {NB5, quarter, quarter + 50}, {NC6, quarter, quarter + 50},
+    {NA5, half, half + 50}, {REST, 2 * half, 0}
+  };
+
+int timeToStartPlaying;
+
+// THIS VARIABLE SHOULD BE TIME BETWEEN LED's flashing
+void startPlaying(int numBytes) {
+  timeToStartPlaying = millis() + Wire.read();
+  Serial.println(timeToStartPlaying);
+  menuState = 3;
+}
+
 void updateButtons() {
-  for(int i = 0; i < numButtons; i++)
+  for(int i = 0; i < numButtons; i++) {
     buttonStatuses[i] = HIGH - digitalRead(buttonPins[i]);
+  }
 }
 
 void pickSong() {
@@ -28,7 +90,7 @@ void pickSong() {
   
   if(buttonStatuses[0]) { // play Bella Ciao
     chosenSong = 1;
-    menuState = inGame;
+    menuState = 2;
     resetScreen();
     lcd.print("Playing");
     lcd.setCursor(0, 1);
@@ -38,7 +100,7 @@ void pickSong() {
 
   if(buttonStatuses[1]) { // play Toxic
     chosenSong = 2;
-    menuState = inGame;
+    menuState = 2;
     resetScreen();
     lcd.print("Playing Toxic");
     delay(2500);
@@ -46,7 +108,20 @@ void pickSong() {
 }
 
 void playGame() {
-  // idle
+  while(millis() < timeToStartPlaying)
+    Serial.println(String(millis()) + ", " + timeToStartPlaying);
+  
+  for (int i = 0; i < 44; i++) {
+    if (BellaCiao[i][0] == REST) {
+      int dur = BellaCiao[i][1];
+      delay(dur);
+    } else {
+      int note = BellaCiao[i][0]; 
+      int dur = BellaCiao[i][1];
+      tone(BuzzerPin, note, dur);
+      delay(BellaCiao[i][2]);
+    }
+  }
 }
 
 void resetScreen() {
@@ -67,16 +142,15 @@ void startScreen() {
   for (int i=0; i<numButtons; ++i) {
       if(buttonStatuses[i] == HIGH) {
         resetScreen();
-        menuState = pickingSong;
+        menuState = 1;
         delay(200);
      }
   }
 }
 
 void sendButtonConfig(int numBytes) {
-  Serial.println(String(numBytes) + ", " + String(menuState) + ", " + String(chosenSong));
   if (numBytes == 4) {
-    if (menuState == inGame) {
+    if (menuState == 2) {
       Wire.write(chosenSong); // Tell the main Arduino that we are ready to play
     } else {
       Wire.write(0);
@@ -90,6 +164,7 @@ void sendButtonConfig(int numBytes) {
 void setup() {
   Wire.begin(2);
   Wire.onRequest(sendButtonConfig);
+  Wire.onReceive(startPlaying);
   
   lcd.begin(16, 2);  
   
@@ -97,20 +172,15 @@ void setup() {
 
   for(int i = 0; i < numButtons; i++)
     pinMode(buttonPins[i], INPUT);
-
 }
 
 void loop() {
   updateButtons();
-  switch(menuState) {
-    case start: 
-      startScreen();
-      break;
-    case pickingSong:
-      pickSong();
-      break;
-    case inGame: 
+//  if(menuState == 0)
+//      startScreen();
+//  else if (menuState == 1)
+//      pickSong();
+//  else 
+  if (menuState == 3) 
       playGame();
-      break;
-  }
 }
--- PlayGameMaster/PlayGameMaster.ino
+++ PlayGameMaster/PlayGameMaster.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 
 const int BuzzerPin = 12;
-const int LEDPins[4][4] = {{1, 13, 11, 10}, {9, 8, 7, 6}, {5, 4, 3, 2}, {A0, A1, A2, A3}};
+const int LEDPins[4][4] = {{A3, A2, A1, A0}, {12, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 13}};
 const int numRows = sizeof(LEDPins)/sizeof(LEDPins[0]);
 const int numCols = sizeof(LEDPins[0])/sizeof(LEDPins[0][0]);
 
@@ -14,77 +14,67 @@ bool isFirstNote = true;
 
 int gameState, chosenSong, chosenSongLength;
 
-// SONG STUFF
-const int REST = 100; // has to be unique
-
-// 3rd Octave
-const int NC3 = 131; const int NCS3 = 138; const int ND3 = 147; const int NDS3 = 155;
-const int NE3 = 165; const int NF3 = 175; const int NFS3 = 185; const int NG3 = 196;
-const int NGS3 = 208; const int NA3 = 220; const int NAS3 = 233; const int NB3 = 247;
-
-// 4th Octave (middle C)
-int NC4 = 262; int NCS4 = 277; int ND4 = 294;
-int NDS4 = 311; int NE4 = 330; int NF4 = 349;
-int NFS4 = 370; int NG4 = 392; int NGS4 = 415;
-int NA4 = 440; int NAS4 = 466; int NB4 = 494;
-
-// 5th Octave
-int NB5 = 988; int NAS5 = 932; int NA5 = 880;
-int NGS5 = 830; int NG5 = 784; int NFS5 = 740;
-int NF5 =  698; int NE5 =  659; int NDS5 = 622;
-int ND5 = 587; int NCS5 = 554; int NC5 = 523;
-
-// 6th Octave
-
-int NB6 = 1975; int NAS6 = 1865; int NA6 = 1760;
-int NGS6 = 1661; int NG6 = 1568; int NFS6 = 1480;
-int NF6 = 1397; int NE6 = 1318; int NDS6 = 1244;
-int ND6 = 1175; int NCS6 = 1109; int NC6 = 1046;
-
 // define the notes durations
-int half, dottedHalf, quarter, dottedQuarter, eighth, dottedEighth, sixteenth;
+int song_bpm = 120; 
+int quarter = 60000 / song_bpm;
+int dottedQuarter = quarter * 1.5;
+int eighth = quarter / 2;
+int dottedEighth = eighth * 1.5;
+int half = 2 * quarter;
+int dottedHalf = 3 * quarter;
+int sixteenth = eighth / 2;
 
-int bella_ciao_bpm = 120; 
-int toxic_bpm = 144;
+void lightLED(int row, int col) {
+  int LEDPin = LEDPins[row][col];
+  digitalWrite(LEDPin, HIGH);
+  currentlyLitLEDs[row][0] = LEDPin;
+  currentlyLitLEDs[row][1] = col;
+}
 
-void defineNoteDurations(int song_bpm) {
-  quarter = 60000 / song_bpm;
-  dottedQuarter = quarter * 1.5;
-  eighth = quarter / 2;
-  dottedEighth = eighth * 1.5;
-  half = 2 * quarter;
-  dottedHalf = 3 * quarter;
-  sixteenth = eighth / 2;
-  // THIS VARIABLE SHOULD BE TIME BETWEEN LED's flashing
+void unlightLED(int row, int col) {
+  int LEDPin = LEDPins[row][col];
+  digitalWrite(LEDPin, LOW);
+  currentlyLitLEDs[row][0] = 0;
+  currentlyLitLEDs[row][1] = 0;
 }
 
 void playGame() {
-  defineNoteDurations(bella_ciao_bpm);
+  int BCButtonMappings[75] = {0, 1, 3, 0, 1, -1, -1, -1, 0, 1, 3, 0, 1, -1, -1, -1, 
+  0, 1, 3, 0, -1, 3, 1, 0, -1, 3, 1, 
+  0, -1, 0, -1, 0, -1, 2, 0, 1, 1, -1, -1, -1, 
+  1, 0, 2, 1, 0, -1, -1, -1, -1, 
+  0, 2, 0, -1, 3, -1, 0, -1, 3, -1, 0, -1, 1, -1};
+  int currentIndex = 0;
+
+  while (BCButtonMappings[currentIndex] == -1)
+    currentIndex++;
+
+  // currentIndex now points to the first note
+  lightLED(0, BCButtonMappings[currentIndex]);
+  currentIndex++;
+
+  Wire.beginTransmission(2);
+  Wire.write(eighth * 7);
+  Wire.endTransmission();
   
-  int BellaCiao[44][3] = {
-    {NE5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NB5, eighth, eighth + 50}, {NC6, eighth, eighth + 50},
-    {NA5, eighth, eighth + 50}, {REST, dottedQuarter, 0}, {NE5, eighth, eighth + 50}, {NA5, eighth, eighth + 50},
-    {NB5, eighth, eighth + 50}, {NC6, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {REST, dottedQuarter, 0},
+  delay(eighth);
 
-    {NE5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NB5, eighth, eighth + 50}, {NC6, quarter, quarter + 50}, // last one is 1.3. CIAO
-    {NB5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NC6, quarter, quarter + 50},
-    {NB5, eighth, eighth + 50}, {NA5, eighth, eighth + 50}, {NE6, eighth, quarter + 50}, {NE6, eighth, quarter + 50}, {NE6, eighth, quarter + 50}, // CIAO CIAO CIAO. 24 notes
-    {ND6, eighth, eighth + 50}, {NE6, eighth, eighth + 50}, {NF6, eighth, eighth + 50}, {NF6, quarter, quarter + 50}, {REST, half, 0},
-    {NF6, eighth, eighth + 50}, {NE6, eighth, eighth + 50}, {ND6, eighth, eighth + 50}, {NF6, eighth, eighth + 50}, {NE6, quarter, quarter + 50}, {REST, half, 0},
-    {NE6, eighth, eighth + 50}, {ND6, eighth, eighth + 50}, {NC6, eighth, eighth + 50}, {NB5, quarter, quarter + 50}, {NE6, quarter, quarter + 50}, {NB5, quarter, quarter + 50}, {NC6, quarter, quarter + 50},
-    {NA5, half, half + 50}, {REST, 2 * half, 0}
-  };
-
-  for (int i = 0; i < 44; i++) {
-    if (BellaCiao[i][0] == REST) {
-      int dur = BellaCiao[i][1];
-      delay(dur);
-    } else {
-      int note = BellaCiao[i][0]; 
-      int dur = BellaCiao[i][1];
-      tone(BuzzerPin, note, dur);
-      delay(dur);
+  for(int i = currentIndex; i < sizeof(BCButtonMappings) / sizeof(BCButtonMappings[0]); i++) {
+    for (int j = numRows - 1; j >= 0; j--) {
+      if (currentlyLitLEDs[j][0] != 0) {
+        // There is currently an LED lit, shift it down
+        int thisLEDCol = currentlyLitLEDs[j][1];
+        unlightLED(j, thisLEDCol);
+        if (j < numRows - 1)
+          lightLED(j + 1, thisLEDCol);
+      }
     }
+
+    int noteToAdd = BCButtonMappings[i];
+    if (noteToAdd != -1)
+      lightLED(0, noteToAdd);
+
+    delay(eighth);
   }
 }
 
@@ -99,7 +89,7 @@ void setup() {
     for (int j = 0; j < numCols; j++)
       pinMode(LEDPins[i][j], OUTPUT);
 
-  gameState = 0;
+  gameState = 1;
 }
 
 void loop() { 
@@ -116,14 +106,4 @@ void loop() {
       gameState = 1;
     }
   }
-}
-
-void playSong1() {
-  Serial.println("Song 1");
-  delay(1000);
-}
-
-void playSong2() {
-  Serial.println("Song 2");
-  delay(1000);
 }
